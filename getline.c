0300b8345215c5c1dcd99702915a497de03fc556
GiftAlufandika <michyleramdass@gmail.com>
2023-09-20 11:20:35 -0700

    getline

---
+++ getline.c
@@ -0,0 +1,68 @@
+#include <shell.h>
+
+/**
+ * get_line - This will read a line from standard input
+ *
+ * Return: A pointer to the line, NULL if an error occurred
+ * or end of file is reached
+ */
+char *get_line(void)
+{
+	static char buffer[BUFFER_SIZE];
+	static size_t position = 0;
+	static size_t size =0;
+
+	char *line = NULL;
+	size_t len = 0;
+
+	while (1)
+       	{
+		if (position >= size)
+		{
+			size = read(STDIN_FILENO, buffer, BUFFER_SIZE);
+			if (size == 0)
+			{
+				return NULL;
+			}
+			else
+				if (size < 0)
+				{
+					perror("read");
+					exit(EXIT_FAILURE);
+				}
+			position = 0;
+		}
+		if (buffer[position] == '\n' || buffer[position] == '\0')
+		{
+			line = malloc(len +1);
+			if (!line)
+			{
+				perror("malloc");
+			}
+			exit(EXIT_FAILURE);
+		}
+		len++
+
+		position++
+	}
+}
+
+int main(void)
+{
+	char *line;
+
+	while (1) 
+	{
+		print("$ ");
+		line = get_line();
+		if (line == NULL)
+		{
+			printf("\n");
+			break
+		}
+		printf("You entered: %s\n", line);
+		free(line);
+	}
+
+	return 0;
+}
--- shell.h
+++ shell.h
@@ -1,6 +1,17 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
 /**
  *shell.h - Header file for a simple shell program.
  *
@@ -11,6 +22,8 @@
 void execute_command(const char *command);
 ssize_t read_command(char *buffer, size_t size);
 extern void exit_builtin();
+
+#define BUFFER_SIZE 1024
 
 #endif
 
